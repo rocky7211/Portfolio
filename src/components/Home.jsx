3f78789ddb00075f80baee56335e08c79adb975b
rocky7211 <jared.mcdowall@outlook.com>
2024-11-11 09:43:13 +1300

    #59

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
                 <p className="font-bold">Kia ora! My name is</p>
                 <h1 className="text-4xl sm:text-7xl font-bold text-[#FD00E3]">Jared McDowall</h1>
-                <h2 className="text-2xl sm:text-5xl font-bold">Computer Science Major</h2>
+                <h2 className="text-2xl sm:text-5xl font-bold">Computer Science Graduate</h2>
                 <p className="font-bold py-4 max-w-[700px]">Always on a mission to learn and develop technically and personally. 
                 Passionate about making a positive difference to the world through technology.</p>
                 <div>
