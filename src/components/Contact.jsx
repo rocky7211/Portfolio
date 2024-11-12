a0ea681f1aaa569c134079e788ec4882dba1a2fd
rocky7211 <jared.mcdowall@outlook.com>
2024-11-12 19:56:20 +1300

    #65

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div name="contact" className="w-full h-screen bg-[#08314A] flex justify-center items-center p-4 text-white">
+        <div name="contact" className="w-full min-h-screen bg-[#08314A] flex justify-center items-center p-4 text-white">
             <form method="POST" action="https://getform.io/f/40272515-b19c-463c-9776-22431545874c" className="flex flex-col max-w-[600px] w-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-[#FD00E3]">Contact</p>
